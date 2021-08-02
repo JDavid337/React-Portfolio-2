2c219d96363e6f62e27ca526459d68b1743de3eb
Captain Ahab <captainahab337@gmail.com>
2021-08-01 23:35:35 -0600

    Working on Portfolio Page and separate Card components

--- client/src/pages/Portfolio.js
+++ client/src/pages/Portfolio.js
@@ -4,6 +4,7 @@ function Portfolio() {
     return (
         <div>
             <h1>hello from the portfolio page</h1>
+            <h2 href='https://express-note-jotter-jzd.herokuapp.com'></h2>
         </div>
     )
 }
